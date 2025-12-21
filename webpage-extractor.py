5ff1cba00ebb28a1acf963ac6fa27ed6de018cb6
jshilts <jarrod.shilts@gmail.com>
2025-12-21 16:20:08 +0000

    revised the venv virtual environment (hidden by gitignore)

---
+++ webpage-extractor.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# > cd "C:\Users\jshilts\OneDrive\Documents\Other Documents\Programming\Python\out-the-pipeline"
+# >  .\.venv\Scripts\activate
+# then set  Tools>Preferences>Python Intepreter  to  C:\Users\jshilts\OneDrive\Documents\Other Documents\Programming\Python\out-the-pipeline\.venv\Scripts\python.exe
